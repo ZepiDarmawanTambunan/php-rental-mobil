f13a1e4d682b9003814076fdca50c56441e7e739
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-23 13:15:47 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+if(!defined('BASEPATH')) echo "Dilarang mengakses ke halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'http:localhost:8000');
+define('APP_NAME', 'Aplikasi Rental Mobil');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'php-rental-mobil');
+define('DB_USER', 'root');
+define('DB_PASS', '');
